80a720ddddff7f8bf8909eb6a879ef71504a3236
Mateus Gutemberg <mateus.gut432@gmail.com>
2024-11-24 19:38:01 -0300

    printed the id and title for each page of the xml file

--- lab1.py
+++ lab1.py
@@ -13,4 +13,11 @@ for child in root:
     counting_pages += 1
 print(counting_pages)
 
-
+#second step: print the id and the title of each tag at the xml file
+counting_pages = 0
+for child in root:
+    counting_pages += 1
+    node = child.iter()
+    for n in node:
+        if n.tag != 'text' and n.tag != 'page':
+            print(counting_pages, n.tag, n.text)
