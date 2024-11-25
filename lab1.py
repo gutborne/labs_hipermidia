23ff7b7937952160f86de08ba2151ac4452e3831
Mateus Gutemberg <mateus.gut432@gmail.com>
2024-11-25 10:01:12 -0300

    added some useful functions from the doccumentation

--- lab1.py
+++ lab1.py
@@ -14,10 +14,31 @@ for child in root:
 print(counting_pages)
 
 #second step: print the id and the title of each tag at the xml file
-counting_pages = 0
+count = 0
+"""
 for child in root:
-    counting_pages += 1
+    count += 1
     node = child.iter()
     for n in node:
         if n.tag != 'text' and n.tag != 'page':
-            print(counting_pages, n.tag, n.text)
+            print(count, n.tag, n.text)
+"""
+#some useful functions from the etree documentation
+#this is being a different way to do the second step 
+"""
+for i in range(counting_pages):
+    for j in range(2):
+        print(root[i][j].text)
+"""
+#how to use iter()
+"""for title in root.iter('title'):
+    print(title.text)
+"""
+#this is being a different way to do the second step using findall and find
+"""
+for page in root.findall('page'):
+    count += 1
+    id = page.find('id').text
+    title = page.find('title').text
+    print(f"{count}{' '}{id}{' '}{title}")
+"""
