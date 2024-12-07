10f6207b88f35d19c54b4a7a336f8e4d05846f67
Mateus Gutemberg <mateus.gut432@gmail.com>
2024-12-07 00:02:41 -0300

    almost finishing the seventh step

--- lab1.py
+++ lab1.py
@@ -109,7 +109,7 @@ escrever_out_file(dict_data_pages)
 """
 #sixth step: remove the stop words
 print("\n                                     sixth step".upper())
-user_words_in_text = 0
+"""user_words_in_text = 0
 total_words_in_text = 0
 user_word_percentage = 0
 dict_title_occur = {}
@@ -143,5 +143,85 @@ def percentage_getter(tuple):
 dict_data_pages.sort(key=percentage_getter, reverse=True)
 for i in range(100):
     print(dict_data_pages[i])
+"""
 
+#seventh step: cache the queries
+print("\n                                     seventh step".upper())
+class Result:
+    keyword = ""
+    results = []
+    def __str__(self):
+        return f"word: {self.keyword} \n queries: \n {self.results}"
+
+#child 
+
+
+
+def percentage_calculator(page, user_defined_str):
+    words_in_text = 0 #user-defined string in the text
+    total_words = 0 #total words in the text of the page in which the len(word) >= 4 
+    user_word_perc = 0
+    text_words = page.find('text').text.split()
+    title_words = page.find('title').text.split()
+    for word in text_words:
+        if(len(word) >= 4):
+            total_words += 1
+        if(word.lower() == user_defined_str.lower()):
+            words_in_text += 1
+    user_word_perc = round(words_in_text/total_words * 100, 3) #visualize the data better
+    for word in title_words:
+        if(user_defined_str.lower() == word.lower()):
+            user_word_perc = round(user_word_perc + (0.1 * user_word_perc), 3)
+            break
+    return user_word_perc
+
+def is_cache_empty():
+    if(len(cache_memory) == 0):
+        return True
+    else:
+        return False
     
+def is_keyword_not_found(cache_memory, user_def_str):
+    if(is_cache_empty()):
+        return True
+    else:#if the cache isn't empty
+        for result in cache_memory:
+            if(result.keyword.lower() == user_def_str.lower()):
+                return True
+        return False
+
+def percentage_getter(tuple):
+            return tuple[0]
+def print_result(result):
+    for i in range(100):
+        print(result[i])      
+
+cache_memory = []#list of objects in which each one has a keyword and list of results
+user_word_perc = 0
+result = Result()
+answer = int(input("would you like to search? "))
+while(answer == 1):
+    print("type a word: ")
+    defined_str = input() #user-defined string
+    while(len(defined_str) < 4):
+        print("wrong length! type again!")
+        defined_str = input("type a word: ")
+    result.keyword = defined_str
+    cache_is_empty = is_cache_empty()
+    is_search_not_found = is_keyword_not_found(cache_memory, defined_str)#cache isn't empty, but even so the word searched wasn't found
+    if(cache_is_empty or is_search_not_found):
+        print("will read the xml page!\n")
+        for page in root.findall('page'):
+            title = page.find('title').text
+            id = page.find('id').text 
+            user_word_perc = percentage_calculator(page, defined_str)
+            result.results.append((user_word_perc, title, id))
+        result.results.sort(key=percentage_getter, reverse=True)
+        cache_memory.append(result)
+        print_result(result.results)
+    else:
+        print("going to find the word on cache!\n")
+        for res in cache_memory:
+            if(res.keyword.lower() == user_defined_str.lower()):
+                print_result(res)
+    answer = int(input("would you like to search again? "))
